e364092cd332cffa60f4b244dd350ee58609a1b5
Donatas Noreika <donoras@gmail.com>
2022-12-01 10:19:15 +0200

    +Padaryti vartotojo prisijungimą (registracija) - papildyta

--- mysite/orders/urls.py
+++ mysite/orders/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MyOrderListView.as_view(), name='orders'),
+    path("", views.OrderListView.as_view(), name='orders'),
 ]
--- mysite/orders/views.py
+++ mysite/orders/views.py
@@ -3,7 +3,9 @@ from django.views import generic
 from .models import MyOrder
 
 # Create your views here.
-class MyOrderListView(generic.ListView):
+class OrderListView(generic.ListView):
     model = MyOrder
     context_object_name = "orders"
     template_name = "orders.html"
+
+
